7668206e38e9f83d8d89e8e06c694f32471cf8ea
jdncodes <181788284+jdncodes@users.noreply.github.com>
2025-05-11 00:55:46 +0300

    added rotations, updated constants/main/player.py

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
 from constants import *
+from player import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
-    pygame.init
+    pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick() / 1000
-        #dt = clock.get_time() / 1000
+        dt = clock.get_time() / 1000
+        #dt = clock.tick(60) / 1000
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
